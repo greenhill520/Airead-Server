b0d7b80305203b691fe2afe21af65f687092aaec
ragnraok <okone1288@gmail.com>
2013-04-07 22:58:42 +0800

    deleted:    .manager.py.swp 	deleted:    airead/.config.py.swp 	deleted:    airead/api/.feed_api.py.swp 	deleted:    airead/api/auth/digestauth.py~ 	deleted:    airead/feeds/.feed_fetch.py.swp 	deleted:    airead/feeds/feed_getter.py 	deleted:    airead/models/.feed.py.swp 	deleted:    airead/models/.user.py.swp 	deleted:    airead/syncdb.py

--- airead/syncdb.py
+++
@@ -1,7 +0,0 @@
-from airead.app import app
-app.config.from_pyfile("config.py")
-
-from airead.models import User, UserSubscribe, FeedArticle, FeedSite
-from airead.database import db
-
-db.create_all()
