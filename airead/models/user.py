0069e6b941795cb39c9173cb982eb422576d58ec
ragnraok <okone1288@gmail.com>
2013-04-08 00:16:41 +0800

    little fixed for user models

--- airead/models/user.py
+++ airead/models/user.py
@@ -9,9 +9,9 @@ class User(db.Model):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
+    username = db.Column(db.String(120), nullable=False, unique=True)
     email = db.Column(db.String(120), nullable=False, unique=True)
-    password = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
     def __init__(self, username, email, password):
         self.username = username
@@ -118,8 +118,8 @@ class AdminUser(db.Model):
     __tablename__ = 'adminuser'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(20), nullable=False) 
+    username = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(120), nullable=False) 
 
     def __init__(self, username, password):
         self.username = username
